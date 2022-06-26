50b51ff06c6154f385ed39dd720a2b413afa254b
Jin Wang <jin.wang@affirm.com>
2022-06-26 15:23:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-# TODO
-# link to oh-my-zh http://dimafeng.com/2016/10/09/dotfiles/
-
-# run on installation
-mkdir ~/.logs
+# Install ohmyzsh
 sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install fzf
-# To install useful key bindings and fuzzy completion:
+
+# Install command line tools
+brew install fzf ag jq
+
+# Install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install

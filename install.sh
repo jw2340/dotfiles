0886f0a78cabed311e7e53fab78da10fd1dcc902
Jin Wang <jin.wang@affirm.com>
2022-08-06 18:54:18 -0400

    Install homebrew in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+# Install homebrew
+if ! command -v brew; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew already exists. Skipping..."
+fi
+
 # Install ohmyzsh
 sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 

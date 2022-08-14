7b6d3ce779c093b88a5ada0f5cc451bbfecbf25d
Jin Wang <jin.wang.527@gmail.com>
2022-08-13 20:18:47 -0400

    Install kubectl in install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install command line tools
-brew install fzf ag jq
+brew install fzf ag jq kubectl
 
 # Install useful key bindings and fuzzy completion:
 $(brew --prefix)/opt/fzf/install
